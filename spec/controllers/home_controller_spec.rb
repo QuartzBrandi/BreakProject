a61a9cbc3af1902a7773cfe05b0fd80dd439366d
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-08 10:48:18 -1000

    Edited index rspec tests.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,29 +2,37 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe "#index" do
-    it "is successful" do
+    before :each do
       get :index
+    end
+
+    it "is successful" do
       expect(response).to be_success
       expect(response.status).to eq 200
       expect(response).to have_http_status 200
     end
 
     it "renders the index page" do
-      get :index
       expect(response).to render_template :index
     end
 
     context "with no session" do
-      it "assigns 'library' to an empty array" do
-        get :index
+      it "assigns 'library' to nil" do
         expect(assigns(:library)).to eq nil
       end
     end
 
     context "with a session" do
+      before :each do
+        session[:player_id] = "NOT DETEREMINED YET"
+      end
+
       it "assigns 'library' to player's games" do
-        get :index
         expect(assigns(:library)).to eq ["RESULTS"]
+      end
+
+      it "assigns 'library' to empty array if no games" do
+        expect(assigns(:library)).to eq []
       end
     end
   end
