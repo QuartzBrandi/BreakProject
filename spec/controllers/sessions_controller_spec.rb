baebd83027c2070e192f1e772cb37e9534866ade
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-08 11:09:08 -1000

    Added sessions tests.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe "#new" do
+    it "creates a new session" do
+      post :new
+      expect(session[:player_id]).to eq 1
+    end
+  end
 
+  describe "#destroy" do
+    it "destories an existing session" do
+      delete :destroy
+      expect(session[:player_id]).to eq nil
+    end
+  end
 end
