4c350014b7e5ac5330721c4dfe0e6dceccb2b1f5
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-09 10:21:32 -1000

    Drafted out specs for Steam API.

--- spec/lib/steam_api_spec.rb
+++ spec/lib/steam_api_spec.rb
@@ -1,10 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe SteamAPI do
-  describe "#some method" do
-    it "returns something" do
-      result = SteamAPI.some_method("fish")
-      expect(result).to eq "hsif"
+  describe "#get_game_population" do
+    it "takes a string (appid) and an argument" do
+
+    end
+
+    it "returns an integer" do
+
+    end
+
+    it "returns -1 if not a game on Steam" do
+
+    end
+  end
+
+  describe "#get_player_library" do
+    it "takes a string (Steam ID) as an argument" do
+
+    end
+
+    it "returns a hash of games (appid: {name, img_logo_url})" do
+      # appid + name + img_logo_url
+      # {440: {name: 'Team Fortress 2', img_logo_url: 'img'}}
+
+    end
+
+    it "returns an empty hash if user is private" do
+
+    end
+
+    it "returns an empty hash if user owns no games" do
+
+    end
+  end
+
+  describe "#get_steam_ID" do
+    it "takes a string as an argument" do
+
+    end
+
+    it "returns a string (of numbers)" do
+
+    end
+
+    it "returns '-1' if not a valid Steam username" do
+
     end
   end
 end
