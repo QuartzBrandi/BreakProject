57c06879b78261138e23767d3b341fac985277c4
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-09-10 12:55:19 -1000

    Added more sessions specs & got factory girl to work.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,16 +2,33 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "#create" do
-    it "creates a new session" do
+    before :each do
+      user = create(:player)
       post :create
+    end
+
+    it "creates a new session" do
       expect(session[:player_id]).to eq 1
+    end
+
+    it "redirects to the home page" do
+      expect(response).to redirect_to url_for(controller: :home, action: :index)
     end
   end
 
   describe "#destroy" do
+    before :each do
+      user = create(:player)
+      session[:player_id] = user.id
+      post :destroy
+    end
+
     it "destories an existing session" do
-      delete :destroy
       expect(session[:player_id]).to eq nil
+    end
+
+    it "redirects to the home page" do
+      expect(response).to redirect_to url_for(controller: :home, action: :index)
     end
   end
 end
